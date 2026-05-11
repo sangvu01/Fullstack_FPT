820948ca6f4f7d9d6ca4a5231b1ba5817efcdd91
sangvu01 <vuminhsang01@gmail.com>
2026-05-11 15:02:52 +0700

    init git; created GET POST PUT DELETE Apis in BE; start to do FE

---
+++ backend/src/routes/todoRoutes.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+
+const Todo = require("../models/Todo");
+
+router.get("/", async (req, res) => {
+    try {
+        const todos = await Todo.find();
+
+        res.json(todos);
+    }
+
+    catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+
+
+router.post("/", async (req, res) => {
+    try {
+        const newTodo = new Todo({
+            text: req.body.text
+        });
+
+        const savedTodo = await newTodo.save();
+
+        res.status(201).json(savedTodo);
+    }
+
+    catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+})
+
+
+router.delete("/:id", async (req, res) => {
+    try{
+        await Todo.findByIdAndDelete(req.params.id);
+
+        res.json({
+            message: "DELETED"
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+
+router.put("/:id", async (req, res) => {
+    try{
+        const updatedTodo = await Todo.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true
+            }
+        );
+
+        res.json(updatedTodo);
+    }
+
+    catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+
+
+module.exports = router;
