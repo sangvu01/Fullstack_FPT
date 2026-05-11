23cc3af77ab18b85486fdeff7405818c52b1b629
sangvu01 <vuminhsang01@gmail.com>
2026-05-11 16:49:09 +0700

    add 'ADD' and 'DELETE' features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,52 @@ import { useEffect } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import './App.css'
+// import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const BE_URL = "http://localhost:5000/todos"
+
+
+
 
   useEffect(() => {
     fetchTodos();
   }, []);
 
+
+  const addTodo = async () => {
+
+    if (!input.trim()) return;
+
+    try{
+      await fetch(BE_URL, {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+          text: input
+        })
+      });
+
+      setInput("");
+
+      fetchTodos();
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+
+
   const fetchTodos = async () => {
     try{
-      const response = await fetch("http://localhost:5000/todos")
+      const response = await fetch(BE_URL)
 
       const data = await response.json();
 
@@ -25,16 +59,57 @@ function App() {
     }
   }
 
+
+  const deleteTask = async (_id) =>{
+    try{
+      // await fetch(BE_URL + '/' + _id, {
+      await fetch(`${BE_URL}/${_id}`, {
+        method: "DELETE"
+      })
+
+      fetchTodos();
+    }
+
+    catch(err){
+      console.log(err)
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <>
       <div>
         Sangvu - TodoList
       </div>
 
+      <input
+        type='text'
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        placeholder='Enter new task...'
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+
       {
         todos.map((todo) => (
           <div key={todo._id}>
             <p>{todo.text}</p>
+            <button onClick={() => deleteTask(todo._id)}>DELETE</button>
           </div>
         ))
       }
